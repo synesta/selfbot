feca4734c2b229b344c861f3e89811486efece05
synesta#5526 <neptoonicals@gmail.com>
2021-02-05 18:28:54 -0500

    Update
    Code cleanup
    Commenting

--- selfbot.js
+++ selfbot.js
@@ -1,17 +1,15 @@
-const Discord = require("discord.js");
+const Discord = require("discord.js"); 
 const client = new Discord.Client();
-const chalk = require("chalk");
+const chalk = require("chalk"); // used for changing colors in console.log
 const fs = require("fs");
-var config = require("./config.json");
-const log = console.log;
-process.title = "selfbot terminal | https://locus.solutions";
-
-
-const { prefix, token, sbversion, footer, response } = config;
+var config = require("./config.json"); // you will use this for everything included in line 7
+const log = console.log; // variable 
+const { prefix, token, sbversion, footer } = config;
 const Constants = require("discord.js/src/util/Constants.js");
 const EventEmitter = require("events");
+process.title = "selfbot terminal | https://locus.solutions"; // changes name of the terminal
 
-
+// logging
 client.on("ready", () => {
 	client.login(config.token).catch(console.error);
 	log(chalk.greenBright("[LOGIN] Successfully logged in with token as : " +client.user.username));
@@ -20,7 +18,6 @@ client.on("ready", () => {
 	log(chalk.blue("[NOTICE] Using a selfbot is BANNABLE by discord TOS, if you do not want to get banned, do not use this."))
 });
 	
-
 // help
 client.on("message", function(message) {
     if (message.content ==`${prefix}help`) {
@@ -55,21 +52,20 @@ client.on("message", function(message) {
 // chat spam, this command doesn't stop. If you want to stop it restart the selfbot 
 client.on("message", function (message) {	
 	if (message.content == `${prefix}spam`) {	
-		message.delete()
+		message.delete() // deletes message before sending response
 		message.channel.send("spam message")
 		
 
 		var interval = setInterval(function () {	
 			message.channel.send("spam message");	
-		}, 1500); // for conversion, refer to (1 MS (MILLISECOND) = TO 0.001 S (SECOND)
+		}, 1500); // 1500 milliseconds = 1.5 seconds for conversion, refer to (1 MS (MILLISECOND) = TO 0.001 S (SECOND) 
 	}	
 });
-
 
 // party
 client.on("message", function (message) {
     if (message.content ==`${prefix}party`) {
-		message.delete() 
+		message.delete() // deletes message before sending response
 		message.channel.send(`(つ°ヮ°)つ`);
 	
 }
@@ -78,7 +74,7 @@ client.on("message", function (message) {
 // lenny
 client.on("message", function (message) {
     if (message.content ==`${prefix}lenny`) {
-		message.delete()
+		message.delete() // deletes message before sending response
 		message.channel.send("( ͡° ͜ʖ ͡°)");
 
 }
@@ -87,7 +83,7 @@ client.on("message", function (message) {
 // table flip
 client.on("message", function (message) {
     if (message.content ==`${prefix}tableflip`) {
-        message.delete()
+        message.delete() // deletes message before sending response 
 		message.channel.send("(╯°□°）╯︵ ┻━┻");
 	
 }
@@ -96,7 +92,7 @@ client.on("message", function (message) {
 // unflip
 client.on("message", function (message) {
 	if (message.content ==`${prefix}unflip`) {
-        message.delete()
+        message.delete() // deletes message before sending response
 		message.channel.send("┬─┬ ノ( ゜-゜ノ)");
 	
 }
